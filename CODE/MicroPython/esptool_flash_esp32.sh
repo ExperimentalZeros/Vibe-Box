7cda86b17281373d033ef184051a26242b0231de
x <ExperimentalZeros@gmail.com>
2026-03-29 00:19:53 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   CODE/Espruino/test.file
    	renamed:    MicroPython/NeoPixels.py -> CODE/MicroPython/NeoPixels.py
    	renamed:    MicroPython/DS1302.py -> CODE/MicroPython/ds1302.py
    	new file:   CODE/MicroPython/esptool_flash_esp32.sh
    	renamed:    MicroPython/main.py -> CODE/MicroPython/main.py
    	new file:   CODE/MicroPython/mpremote_program_esp32.sh
    	renamed:    MicroPython/rtc.py -> CODE/MicroPython/rtc.py
    	renamed:    MicroPython/vfd.py -> CODE/MicroPython/vfd.py
    	new file:   FIRMWARE/Espruino/test.file
    	new file:   MEDIA/PICTURES/ESP32_breakout_wiring_layout.png
    	new file:   SOFTWARE/Node-RED
    
     Changes not staged for commit:
    	modified:   SOFTWARE/Node-RED (modified content)

---
+++ CODE/MicroPython/esptool_flash_esp32.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#cat ../../REFERENCES/commands.ref
+#Chip is ESP32-D0WD-V3 (revision v3.1)
+
+esptool.py erase_flash
+esptool.py --baud 460800 write_flash 0x1000 ../../FIRMWARE/MicroPython/ESP32_GENERIC-D2WD-20251209-v1.27.0.bin
